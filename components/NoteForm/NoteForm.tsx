82c0822de685aa824fe3509ab24e0aed85602d58
Ann00012 <ann.malchevska@gmail.com>
2026-01-28 17:16:03 +0200

    update path

--- components/NoteForm/NoteForm.tsx
+++ components/NoteForm/NoteForm.tsx
@@ -4,7 +4,7 @@ import css from "./NoteForm.module.css";
 import { createNote } from "@/lib/api";
 import { useRouter } from 'next/navigation';
 import { useMutation } from "@tanstack/react-query";
-import { useNoteDraftStore } from "@/lib/stores/NoteStore";
+import { useNoteDraftStore } from "@/lib/stores/NoteStkjkjkjore";
 
 export type NoteFormValues ={
   title: string;
